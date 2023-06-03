20131d8d31a84a072d348520302571c6d797b6e6
Guhan <guhan27062002@gmail.com>
2023-06-03 21:20:43 +0530

    connected with mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import express from 'express';
-const app = express();
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
 
-app.listen(8000, () => {
+const app = express();
+dotenv.config();
+
+
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("connected to mongodb");
+    } catch(error){
+        console.log(error);
+        throw error
+        
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected");
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("mongoDB connected");
+})
+
+app.listen(8800, () => {
+    connect()
     console.log("Connected to backend!");
 })
