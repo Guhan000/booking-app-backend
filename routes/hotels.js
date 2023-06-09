77d5c9ef54828aa27f2a091fb6a22d78928bc5ea
Guhan <guhan27062002@gmail.com>
2023-06-09 21:26:11 +0530

    added hotelrooms route

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,6 +3,7 @@ import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel, countByCity, countByType } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
+import { getAllRooms } from "../controllers/room.js";
 const router = express.Router();
 
 router.post("/", verifyAdmin, createHotel);
@@ -18,5 +19,7 @@ router.get("/", getAllHotels)
 router.get("/countByCity", countByCity)
 
 router.get("/countByType", countByType)
+
+router.get("/room/:id", getAllRooms)
 
 export default router;
