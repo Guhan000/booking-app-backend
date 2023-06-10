02cb08ad58d6a7747e66055ac9efe3a39dd421e2
Guhan <guhan27062002@gmail.com>
2023-06-10 21:18:11 +0530

    added cors

--- index.js
+++ index.js
@@ -23,15 +23,23 @@ const connect = async () => {
 }
 
 // middlewares
+var allowlist = ['http://localhost:3000']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (allowlist.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+  }
 
 // const corsOptions ={
 //     origin:'http://localhost:3000', 
 //     credentials:true,            
 //     optionSuccessStatus:200
 // }
-// app.use(cors({
-//     origin: "http://localhost:3000"
-// }));
+app.use(cors(corsOptionsDelegate));
 app.use(express.static('build'))
 app.use(cookieParser());
 app.use(express.json());
