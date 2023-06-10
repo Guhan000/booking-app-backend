8cf70037c1fcdefe9db497fe035f9a184c7ebea3
Guhan <guhan27062002@gmail.com>
2023-06-10 14:42:22 +0530

    cors added

--- index.js
+++ index.js
@@ -23,7 +23,12 @@ const connect = async () => {
 }
 
 // middlewares
-// app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use("/auth", authRoute);
